b8def85fdded90ee95bc7ad6c7075d5377e35b79
Greg Scarich <git-hub@web.knobby.ws>
2021-12-25 13:07:30 -0800

    Removed empty upload_controller
    Causing error at Heroku

--- app/controllers/upload_controller.rb
+++
@@ -1,8 +0,0 @@
-# class UploadController < ApplicationController
-#   def upload
-#     myfile = params[:filename]
-#      puts "from Upload controller: #{myfile}"
-#   end
-# end
-
-# This is not being used. Was an attempt
