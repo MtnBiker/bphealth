3ff6d2c5c45c5fb67ffa805a6303530a3d11bc66
Greg Scarich <git-hub@web.knobby.ws>
2021-12-25 13:18:25 -0800

    Deleted  view_bp_time_only.rb
    Not using. Still have view_bp_time_with_zone. Not using this either, but potentially
    
    Trying to get rid of Heroku errors

--- app/controllers/bp_time_onlies_controller.rb
+++ app/controllers/bp_time_onlies_controller.rb
@@ -1,7 +1,7 @@
 class BpTimeOnly < ApplicationController
   def index
    # view_bp_time_onlies name of view
-    @view_bp_time_onlies = ViewBpTimeOnly.all.order("stattime")
+    # @view_bp_time_onlies = ViewBpTimeOnly.all.order("stattime")
   end
 end
 # SWAG
