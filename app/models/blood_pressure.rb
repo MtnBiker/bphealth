b4abe51b18b1457417609a893fc32fa156b722f6
Greg Scarich <git-hub@web.knobby.ws>
2021-12-27 21:58:21 -0800

    SWAG for git push heroku

--- app/models/blood_pressure.rb
+++ app/models/blood_pressure.rb
@@ -1,5 +1,5 @@
 class BloodPressure < ApplicationRecord
   # iOS Health has all values and don't want to reenter them with each export/import. Would wipe out comments
   validates :statdate,  uniqueness: {scope: [:statdate, :systolic, :diastolic ]}
-  has_one :bp_time_with_zone
+  # has_one :bp_time_with_zone # SWAG to turn off and see if heroku push works
 end
