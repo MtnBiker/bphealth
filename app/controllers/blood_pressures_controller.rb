9618f6d7ca26fbdf3c01b6c0c3a14b87ee34c555
Greg Scarich <git-hub@web.knobby.ws>
2022-01-11 21:00:52 -0800

    Works using default location on localhost
    Now try on Heroku
    
    Dates still a mess

--- app/controllers/blood_pressures_controller.rb
+++ app/controllers/blood_pressures_controller.rb
@@ -67,40 +67,6 @@ class BloodPressuresController < ApplicationController
     end
   end
 
-  # Import button triggers this. For importing from export.zip.
-  def import_data
-    def lineNum()
-      caller_infos = caller.first.split(":")
-      # Note caller_infos[0] is file name
-      # caller_infos[1]
-      return "#{caller_infos[1]}"
-    end # definitely has to be here for this method to work
-    blood_pressure_params
-    # params show up anyway
-    # puts "#{lineNum}. params: #{params}" # params: {"authenticity_token"=>"zDyBm6XJD6HZusFWIfNRKJP3NteiIf9mcAQLq2SvTZx4LHg9tGHJtNdYZzLIzvsXH1cjd6CUnrMQesOEpSwfOg", "blood_pressure"=>{"health_exported_zip"=>#<ActionDispatch::Http::UploadedFile:0x00007fbe22a55338 @tempfile=#<Tempfile:/var/folders/f6/59hv1f7923z2rx7yl3hjl8pm0000gn/T/RackMultipart20211224-13925-75ug17.zip>, @original_filename="export 2.zip", @content_type="application/zip", @headers="Content-Disposition: form-data; name=\"blood_pressure[health_exported_zip]\"; filename=\"export 2.zip\"\r\nContent-Type: application/zip\r\n">}, "commit"=>"Import", "controller"=>"blood_pressures", "action"=>"import_data"}
-    # At Heroku: Tempfile:/tmp/RackMultipart20220109-4-4a864y.zip
-    puts "#{lineNum}. params[:blood_pressure][:health_exported_zip]: #{params[:blood_pressure][:health_exported_zip]}" # #<ActionDispatch::Http::UploadedFile:0x00007fef60a042e8>
-    puts "#{lineNum}. params[:blood_pressure][:health_exported_zip.tempfile.path]: #{params[:blood_pressure][:health_exported_zip].tempfile.path}"  # 
-    tempfile_path = params[:blood_pressure][:health_exported_zip].tempfile.path
-    # indexTempfile = params.to_s.index('/var/folders') # setting start of Tempfile location
-  #   puts "#{lineNum}. indexTempfile: #{indexTempfile}"
-  #   # paramSlice will become the path to Tempfile
-  #   tempfile_path = params.to_s.slice!(indexTempfile..450) # slice off the beginning of params
-  #   # puts "#{lineNum}: tempfile_path: #{tempfile_path}"
-  #   indexEnd = tempfile_path.to_s.index('>') - 1 # get end of Tempfile location. Can't do all at once because some `>` at the beginning. Could of course, but this is simpler
-  #   tempfile_path = tempfile_path.to_s.slice!(0..indexEnd) # slice off the end of params and left with path
-  #   puts "\n#{lineNum}: tempfile_path is now the path to the Tempfile for export.zip: #{tempfile_path}"
-  #   # health_exported_zip_file = params[:health_exported_zip].path # need to look at Better if worked using Rails https://api.rubyonrails.org/v7.0.0/classes/ActionDispatch/Http/UploadedFile.html to get this right
-  #   # health_exported_zip_file = "/Users/gscar/Downloads/export.zip" # changed to
-    unzip_import_data(tempfile_path)
-    puts "#{lineNum}. request.request_parameters: #{request.request_parameters}" #  78. request.request_parameters: {"authenticity_token"=>"oCFXN0yI-Pbu0V1cQSVyaFxB61LmBcYAEqPF2sWUeCcSYELXwwME3WOsmfbT5QDzuG9JPkNKLFhx9g0iRhp0dg", "commit"=>"Import"}
-    # puts "params: #{params}" #  params: {"authenticity_token"=>"NBP3…tvOg", "commit"=>"Import", "controller"=>"blood_pressures", "action"=>"import_data"}
-    # # puts blood_pressure[health_exported_zip] # error
-    # export_zip = params[:health_exported_zip]
-    # puts "export_zip: #{export_zip}" # blank now, although are getting to the controller
-
-    export_xml =  "#{ENV['PWD']}/import_staging/apple_health_export/export.xml" # location set in blood_pressure_controller during unzip. Had trouble using /tmp so set. Could I use a /var/folders/ like Tempfile?
-    redirect_to blood_pressures_url # To rerender page after import
   def lineNum()
     caller_infos = caller.first.split(":")
     # Note caller_infos[0] is file name
@@ -108,6 +74,30 @@ class BloodPressuresController < ApplicationController
     return "#{caller_infos[1]}"
   end # couldn't be found in helpers/application_helper.rb
 
+  # unzipping to one folder, but the file of interest ends up one dir down
+  # extract_zip(zip_path, extract_destination)
+  # Above from https://stackoverflow.com/questions/9204423/how-to-unzip-a-file-in-ruby-on-rails
+  def extract_zip(file, destination)
+    # puts "#{lineNum}. Hello from the top of extract_zip in unzip_import_data in blood_pressures_controller. \nfile: #{file}. \ndestination: #{destination}"
+    # # Create folder for the export.xml which goes to /apple_health_export. That's the way the zipped file is structured
+    # FileUtils.mkdir(destination) unless File.exist?(destination) # I thought the following would create this dir, but, and it won't exist now that Rails is creating a special folder for where selected files are moved to (and given a new name)
+    puts "#{lineNum}. file: #{file}.\ndestination: #{destination}"
+    FileUtils.mkdir(destination) unless File.exist?(destination)
+    Zip::File.open(file) do |zip_file|
+      zip_file.each do |f|
+        # puts "#{lineNum}. Extracting f.name: #{f.name}" # assume the name method is part of zip gem
+        fpath = File.join(destination, f.name)
+        # fpath is the output filepath and the first file will be export.xml
+        # puts "#{lineNum}. fpath: #{fpath}" # /Users/gscar/Documents iMac only/Ruby/Rails 7 Trials/bloodpressure/app//tmp/import/apple_health_export/export.xml
+        zip_file.extract(f, fpath) unless File.exist?(fpath)
+        if f.name == "apple_health_export/export.xml" # Stopping once create the export.xml which is all I need
+          puts "#{lineNum}. export.xml was extracted to #{fpath}"
+          break
+        end
+      end
+    end
+  end
+ 
   # Called second by ConvertXML
   def parse_xml(path)
     document = File.open(path) { |f| Nokogiri::XML(f) }
@@ -156,7 +146,6 @@ class BloodPressuresController < ApplicationController
       # Do I need a hash that looks like  [["statdate", "2021-11-16 16:35:30"], ["hr", 75], ["systolic", 136], ["diastolic", 70]] and forget the accu. NO add_to_db does that
       accu << [sys, pair, hr, startDate ]
       # puts "#{lineNum}. accu: #{accu}"
-      
       # accu << (record['value'], pair, hr, record['startDate']) # leave startDate as string?
     end
     puts "#{lineNum}. accu.last (accu.count: #{accu.count}): #{accu.last}" # All of accu: [[ … , ["132", "68", "53", Sun, 14 Nov 2021 09:02:23 -0800], ["153", "80", "51", Mon, 15 Nov 2021 07:31:42 -0800], ["128", "66", "60", Mon, 15 Nov 2021 17:03:23 -0800], ["136", "70", "51", Tue, 16 Nov 2021 08:35:30 -0800]]
@@ -183,12 +172,50 @@ class BloodPressuresController < ApplicationController
     # puts "/lib/tasks/import.rake:#{lineNum}. Found #{records.count} records, will add new to db." #  NoMethodError - undefined method `count' for nil:NilClass:
     puts "#{lineNum}. processing of export.xml complete and final step to add to db using add_to_db."
     add_to_db(records) # 4
-  end
+  end  # Import button triggers this. For importing from export.zip.
 
-  convert_xml(export_xml)
+  def import_data
+    blood_pressure_params
+    # params show up anyway
+    # puts "#{lineNum}. params: #{params}" # params: {"authenticity_token"=>"zDyBm6XJD6HZusFWIfNRKJP3NteiIf9mcAQLq2SvTZx4LHg9tGHJtNdYZzLIzvsXH1cjd6CUnrMQesOEpSwfOg", "blood_pressure"=>{"health_exported_zip"=>#<ActionDispatch::Http::UploadedFile:0x00007fbe22a55338 @tempfile=#<Tempfile:/var/folders/f6/59hv1f7923z2rx7yl3hjl8pm0000gn/T/RackMultipart20211224-13925-75ug17.zip>, @original_filename="export 2.zip", @content_type="application/zip", @headers="Content-Disposition: form-data; name=\"blood_pressure[health_exported_zip]\"; filename=\"export 2.zip\"\r\nContent-Type: application/zip\r\n">}, "commit"=>"Import", "controller"=>"blood_pressures", "action"=>"import_data"}
+    # At Heroku: Tempfile:/tmp/RackMultipart20220109-4-4a864y.zip
+    # puts "#{lineNum}. params[:blood_pressure][:health_exported_zip]: #{params[:blood_pressure][:health_exported_zip]}" # #<ActionDispatch::Http::UploadedFile:0x00007fef60a042e8>
+    # puts "#{lineNum}. params[:blood_pressure][:health_exported_zip.tempfile.path]: #{params[:blood_pressure][:health_exported_zip].tempfile.path}" #
+    tempfile_path = params[:blood_pressure][:health_exported_zip].tempfile.path #   = params[:blood_pressure][:health_exported_zip].path, but more expressive to leave longer version 
+    # indexTempfile = params.to_s.index('/var/folders') # setting start of Tempfile location
+    puts "#{lineNum}. tempfile_path: #{tempfile_path}"
+  #   # paramSlice will become the path to Tempfile
+  #   tempfile_path = params.to_s.slice!(indexTempfile..450) # slice off the beginning of params
+  #   # puts "#{lineNum}: tempfile_path: #{tempfile_path}"
+  #   indexEnd = tempfile_path.to_s.index('>') - 1 # get end of Tempfile location. Can't do all at once because some `>` at the beginning. Could of course, but this is simpler
+  #   tempfile_path = tempfile_path.to_s.slice!(0..indexEnd) # slice off the end of params and left with path
+  #   puts "\n#{lineNum}: tempfile_path is now the path to the Tempfile for export.zip: #{tempfile_path}"
+  #   # health_exported_zip_file = params[:health_exported_zip].path # need to look at Better if worked using Rails https://api.rubyonrails.org/v7.0.0/classes/ActionDispatch/Http/UploadedFile.html to get this right
+  #   # health_exported_zip_file = "/Users/gscar/Downloads/export.zip" # changed to
+    # tempfile_path = "/var/folders/f6/59hv1f7923z2rx7yl3hjl8pm0000gn/T/RackMultipart20220110-56077-ecytji.zip"
+    # unzip_import_data(tempfile_path) # which will unzip and then import the new data
+    require 'zip' # and this does what it needs to do here
+
+    # https://ruby-doc.org/stdlib-2.4.1/libdoc/fileutils/rdoc/FileUtils.html#method-i-makedirs
+    zip_dir = File.dirname(tempfile_path) # try to use same directory
+    destination = zip_dir + "/apple_health_export/"
+    export_xml = destination + "/export.xml" # see below for original
+    File.delete(export_xml) if File.exist?(export_xml) # delete old files since apparently won't overwrite, at least on macOS, but now in a new tmp folder so won't exist
+    puts "#{lineNum}. zip_dir: #{zip_dir}.\ndestination. #{destination}.\nexport_xml: #{export_xml}"
+    # extract_zip(health_exported_zip, unzip_to)
+    extract_zip(tempfile_path, zip_dir) # since this is how the gem is set up or at least the sample
+    puts "#{lineNum}. Hello from the end of unzip_import_data in blood_pressures_controller. So now unzipped and need to read and import."
+    convert_xml(export_xml)
+    puts "#{lineNum}. tempfile_path (working location of copy of export.zip): #{tempfile_path}" # Repeating the tempfile_path so if needed for debugging, although can get lost because log is filled up by the page refresh
+    # puts "#{lineNum}. request.request_parameters: #{request.request_parameters}" #  78. request.request_parameters: {"authenticity_token"=>"oCFXN0yI-Pbu0V1cQSVyaFxB61LmBcYAEqPF2sWUeCcSYELXwwME3WOsmfbT5QDzuG9JPkNKLFhx9g0iRhp0dg", "commit"=>"Import"}
+    # puts "params: #{params}" #  params: {"authenticity_token"=>"NBP3…tvOg", "commit"=>"Import", "controller"=>"blood_pressures", "action"=>"import_data"}
+
+    # export_xml =  "#{ENV['PWD']}/import_staging/apple_health_export/export.xml" # location set in blood_pressure_controller during unzip. Had trouble using /tmp so set. Could I use a /var/folders/ like Tempfile? Using Rails default now
+    redirect_to blood_pressures_url # To rerender page after import
 end
 
-private
+
+  private
   # Use callbacks to share common setup or constraints between actions.
   def set_blood_pressure
     @blood_pressure = BloodPressure.find(params[:id])
@@ -196,44 +223,7 @@ private
 
   # Only allow a list of trusted parameters through.
   def blood_pressure_params
-    params.require(:blood_pressure).permit(:statdate, :systolic, :diastolic, :heartrate, :sourceName, :sourceVersion, :comment, :file, :filename,  :health_exported_zip)
-  end
-
-  def unzip_import_data(health_exported_zip)
-    require 'zip' # and this does what it needs to do here
-    # health_exported_zip = "/Users/gscar/Downloads/export.zip" # Now set in call to this from line 79
-    # unzip_to = "import_staging" # "/tmp/import/"
-    unzip_to = "tmp"
-    # export_xml = unzip_to + "/apple_health_export/export.xml" # see below for original
-    export_xml = unzip_to # see below for original
-    File.delete(export_xml) if File.exist?(export_xml) # delete old files since apparently won't overwrite, at least on macOS
-
-    # unzipping to one folder, but the file of interest ends up one dir down
-    def extract_zip(file, destination)
-      puts "#{lineNum}. Hello from the top of extract_zip in unzip_import_data in blood_pressures_controller. \nfile: #{file}. \ndestination: #{destination}"
-      # Create folder for the export.xml which goes to /apple_health_export. The original iOS export file, export.zip, has the file of interest 'apple_health_export/export.xml'
-      final_destination = destination # + "/apple_health_export/"
-      puts "final_destination: #{final_destination}"
-      # FileUtils.mkdir(destination) unless File.exist?(destination) # I thought the following would create this dir, but
-      FileUtils.mkdir(final_destination) unless File.exist?(final_destination)
-
-      Zip::File.open(file) do |zip_file|
-        zip_file.each do |f|
-          puts "#{lineNum}. f.name: #{f.name}"
-          fpath = File.join(destination, f.name)
-          puts "#{lineNum}. fpath: #{fpath}" # /Users/gscar/Documents iMac only/Ruby/Rails 7 Trials/bloodpressure/app//tmp/import/apple_health_export/export.xml
-          zip_file.extract(f, fpath) unless File.exist?(fpath)
-          break if f.name == "apple_health_export/export.xml" # Stopping once create the export.xml which is all I need
-        end
-      end
-    end
-    # extract_zip(zip_path, extract_destination)
-    # Above from https://stackoverflow.com/questions/9204423/how-to-unzip-a-file-in-ruby-on-rails
-    # https://ruby-doc.org/stdlib-2.4.1/libdoc/fileutils/rdoc/FileUtils.html#method-i-makedirs
-
-    extract_zip(health_exported_zip, unzip_to)
-    puts "#{lineNum}. Hello from the end of unzip_import_data in blood_pressures_controller. So now unzipped and need to read and import."
-    
+    params.require(:blood_pressure).permit(:statdate, :systolic, :diastolic, :heartrate, :sourceName, :sourceVersion, :comment,:health_exported_zip, :file, :filename)
   end
 
   # Not being used. Using helpers as seems more logical to me at the moment.  commented out helper_method at top stops this from being used, but does work if uncommented. Controller is checked first for method
@@ -249,5 +239,4 @@ private
       "text-danger"
     end
   end
-
 end
